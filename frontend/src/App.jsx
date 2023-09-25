d827580fc915423c0031c21086af825623f3ab24
Matthew Muriithi <muriithimatthew@gmail.com>
2023-09-25 16:45:08 -0400

    add content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import { Navigation } from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,6 +20,9 @@ function App() {
         >
           Learn React
         </a>
+        <div className="container">
+          <Button className='btn btn-success'>Hello</Button>
+        </div>
       </header>
     </div>
   );
