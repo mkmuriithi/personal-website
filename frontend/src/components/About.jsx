39f61bd49a41148414549fb7d9f4e387130aa42d
Matthew Muriithi <muriithimatthew@gmail.com>
2023-10-08 21:34:46 -0400

    add About Section

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import Tilt from 'react-parallax-tilt';
+import { motion } from 'framer-motion';
 
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({index, title, icon}) =>{
+
+  // Tilt props
+  const tiltProps = {
+    transitionSpeed: 450,
+    scale: 1.1,
+    glareEnable: true,
+    glareMaxOpacity: 0.45,
+  }
+  return(
+    <Tilt 
+    {...tiltProps} 
+    className="xs:w-[250px] w-full">
+      <motion.div
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+        <div
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+          <img src={icon} alt={title}
+          className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+
+      <motion.p variants={fadeIn("", "", 0.1, 1)} className={`${styles.sectionText} mt-5 text-secondary text-[17px] max-w-3xl leading-[30px]`}>
+        I'm a software engineer based in Toronto, Canada.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+
+    
+    
+    </>
   )
 }
 
-export default About
+export default SectionWrapper(About, "about");
--- frontend/src/components/canvas/Computers.jsx
+++ frontend/src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import CanvasLoader from '../Loader';
 
 
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
 
   const computer = useGLTF('/desktop_pc/scene.gltf');
 
@@ -21,13 +21,29 @@ const Computers = () => {
       castShadow
       shadow-mapSize={1024}
       />
-      <primitive object={computer.scene} scale={0.75} position={[0, -3.25, -1.5]} rotation={[-0.01, -0.2, -0.1]} />
+      <primitive object={computer.scene} scale={isMobile? 0.7 : 0.75} position={[0, -3.25, -1.5]} rotation={[-0.01, -0.2, -0.1]} />
     </mesh>
   )
 }
 
 
 const ComputerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return() => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+  }, []);
+
   return (
     <Canvas 
     frameLoop="demand"
@@ -40,7 +56,7 @@ const ComputerCanvas = () => {
         maxPolarAngle={Math.PI / 2}
         minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile}/>
       </Suspense>
       <Preload all />
     </Canvas>
